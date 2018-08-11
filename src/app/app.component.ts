50644a7255f39994f1cc46b5e865878a483cb7ea
Timothy Curchod <timofeyc@hotmail.com>
2018-08-12 08:54:28 +1000

    Added dot base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sassy-app';
+
+  constructor() { }
+
+  clickDot(arg: string) {
+    console.log('arg',arg);
+  }
+  
 }
